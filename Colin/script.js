98cb71110c2c85bbc5e392d3f22d7e4e158d5a16
Colin de Groot <colindegroot2004@gmail.com>
2024-09-13 10:27:03 +0200

    homepage bouwen
    header klaargezet en meer gelet op stling

--- Colin/script.js
+++ Colin/script.js
@@ -1,21 +1,21 @@
 const swiper = new Swiper(".swiper-container", {
   loop: true,
   initialSlide: 2,
-  slidesPerView: 1,
+  slidesPerView: 1, // aantal zichtbare slides
   spaceBetween: 0,
   centeredSlides: false,
-  navigation: {
+  navigation: { //buttons voor navigeren
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   breakpoints: {
-    // when window width is >= 480px
+    // when window width is >= 480px tablet
     550: {
-      slidesPerView: 2,
+      slidesPerView: 1,
       centeredSlides: false,
       spaceBetween: 0,
     },
-    // when window width is >= 640px
+    // when window width is >= 640px desktop
     900: {
       slidesPerView: 5,
       centeredSlides: true,
